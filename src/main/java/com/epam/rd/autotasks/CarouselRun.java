b5020559a50e27bc58f23be0fec942458c626d55
“Juan <juanp.restrepoc@gmail.com>
2022-11-21 17:30:40 -0500

    intento

--- src/main/java/com/epam/rd/autotasks/CarouselRun.java
+++ src/main/java/com/epam/rd/autotasks/CarouselRun.java
@@ -1,13 +1,29 @@
 package com.epam.rd.autotasks;
 
+import static com.epam.rd.autotasks.DecrementingCarousel.carrousel;
+
 public class CarouselRun {
 
+    int decreaseOperation;
+
+    int maxOperations;
+
+    int position = 0;
+
     public int next() {
-        throw new UnsupportedOperationException();
+        int count = 0;
+        while (count < carrousel.length && carrousel[position %= carrousel.length] <= 0) {
+            position++;
+            count++;
+        }
+        if (count == carrousel.length) return -1;
+        return carrousel[position++]--;
     }
 
     public boolean isFinished() {
-        throw new UnsupportedOperationException();
-    }
+        for (int var: carrousel) {
+            if (var > 0) return false;
+        }
+        return true;    }
 
 }
--- src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
+++ src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
@@ -1,15 +1,38 @@
 package com.epam.rd.autotasks;
 
-public class DecrementingCarousel {
-    public DecrementingCarousel(int capacity) {
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
+public class DecrementingCarousel {
+    boolean isRunning;
+    static int[] carrousel;
+    int addOperations;
+    int decreaseOperation;
+    int maxOperation;
+    int addIndex;
+
+    public DecrementingCarousel(int capacity) {
+        carrousel = new int[capacity];
+        System.out.println(Arrays.toString(carrousel));
     }
 
     public boolean addElement(int element){
-        throw new UnsupportedOperationException();
+        if (isRunning){
+            return false;
+        }else if(element<1){
+            return false;
+        }else if(carrousel.length == addOperations){
+            return false;
+        }else{
+            carrousel[addIndex] = element;
+            addIndex++;
+            System.out.println(Arrays.toString(carrousel));
+        }
+        return true;
     }
 
     public CarouselRun run(){
-        throw new UnsupportedOperationException();
+        isRunning = true;
+        return new CarouselRun( );
     }
 }
